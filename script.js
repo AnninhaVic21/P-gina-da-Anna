8801730f16b5761ddc0531ed06fc2acab5a5e4ee
AnninhaVic21 <annavictoria1818@gmail.com>
2025-05-19 12:12:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+  const divMensagem = document.getElementById("mensagem");
+  divMensagem.textContent = "Seja Bem-Vindo a minha pequena biografia. Espero que aprenda um pouco sobre quem eu sou!";
+  divMensagem.style.marginTop = "10px";
+  divMensagem.style.fontWeight = "bold";
+}
